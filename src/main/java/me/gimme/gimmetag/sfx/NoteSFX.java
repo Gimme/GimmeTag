6276cf9e4bd0783a5d5a1118bc92e20bdd266a1c
Anders Magnusson <anders14magnusson@gmail.com>
2020-09-29 20:27:43 +0200

    Make NoteSFX sounds global by default

--- src/main/java/me/gimme/gimmetag/sfx/NoteSFX.java
+++ src/main/java/me/gimme/gimmetag/sfx/NoteSFX.java
@@ -1,5 +1,6 @@
 package me.gimme.gimmetag.sfx;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Instrument;
 import org.bukkit.Note;
 import org.bukkit.entity.Player;
@@ -8,14 +9,26 @@ import org.jetbrains.annotations.NotNull;
 class NoteSFX extends SFX {
     private Instrument instrument;
     private Note note;
+    private boolean local;
 
-    NoteSFX(@NotNull Instrument instrument, @NotNull Note note){
+    NoteSFX(@NotNull Instrument instrument, @NotNull Note note) {
+        this(instrument, note, false);
+    }
+
+    NoteSFX(@NotNull Instrument instrument, @NotNull Note note, boolean local){
         this.instrument = instrument;
         this.note = note;
+        this.local = local;
     }
 
     @Override
     public void play(@NotNull Player player) {
-        player.playNote(getFrontOfPlayer(player), instrument, note);
+        if (local) {
+            player.playNote(getFrontOfPlayer(player), instrument, note);
+        } else {
+            for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+                p.playNote(getFrontOfPlayer(player), instrument, note);
+            }
+        }
     }
 }
