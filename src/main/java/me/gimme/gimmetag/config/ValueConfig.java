e9d7488e1d9b75249ad01f639d6a4af0e5bfb419
Anders Magnusson <anders14magnusson@gmail.com>
2020-10-10 05:46:59 +0200

    Fix default config values not being used when the key is missing

--- src/main/java/me/gimme/gimmetag/config/ValueConfig.java
+++ src/main/java/me/gimme/gimmetag/config/ValueConfig.java
@@ -1,6 +1,5 @@
 package me.gimme.gimmetag.config;
 
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,7 +17,6 @@ class ValueConfig<T> extends AbstractConfig<T> {
     @NotNull
     @Override
     public T getValue() {
-        // TODO: safe type check?, make sure the config values get loaded on first run?
-        return (T) Objects.requireNonNull(getConfig().get(path, cls));
+        return (T) Objects.requireNonNull(getConfig().getObject(path, Object.class));
     }
 }
