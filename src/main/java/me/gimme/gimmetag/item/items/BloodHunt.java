0248a8e0e5cfb552118638eb0195331e17dbfd06
Anders Magnusson <anders14magnusson@gmail.com>
2020-10-17 17:50:38 +0200

    Add speed boost to blood hunt

--- src/main/java/me/gimme/gimmetag/item/items/BloodHunt.java
+++ src/main/java/me/gimme/gimmetag/item/items/BloodHunt.java
@@ -11,24 +11,31 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Shows players below a certain health level through walls.
+ * Shows players below a certain health level through walls and grants a speed boost.
  */
 public class BloodHunt extends ContinuousAbilityItem {
 
     private static final String NAME = ChatColor.DARK_RED + "Blood Hunt";
     private static final Material MATERIAL = Material.ARROW;
-    private static final List<String> INFO = Collections.singletonList("See low health targets");
+    private static final List<String> INFO = Arrays.asList("See low health targets", "and run faster while near them");
+
+    private static final int POTION_TICKS = 2;
 
     private final Plugin plugin;
     private final double healthThreshold;
     private final double range;
+    private final List<PotionEffect> potionEffects;
 
     /**
      * Creates a new continuous ability item with the specified name, display name, item type and configuration.
@@ -44,6 +51,11 @@ public class BloodHunt extends ContinuousAbilityItem {
         this.range = range;
 
         setInfo(INFO);
+        if (getAmplifier() != -1) {
+            potionEffects = Collections.singletonList(new PotionEffect(PotionEffectType.SPEED, POTION_TICKS, getAmplifier(), false, false));
+        } else {
+            potionEffects = new ArrayList<>();
+        }
     }
 
     @Override
@@ -54,9 +66,11 @@ public class BloodHunt extends ContinuousAbilityItem {
         double rangeSquared = range * range;
 
         return new ContinuousUse() {
+            private List<Player> targets;
+
             @Override
             public void onCalculate() {
-                List<Player> targets = user.getWorld().getPlayers().stream()
+                targets = user.getWorld().getPlayers().stream()
                         .filter(p -> user.getLocation().distanceSquared(p.getLocation()) < rangeSquared)
                         .filter(p -> {
                             AttributeInstance maxHealthAttribute = p.getAttribute(Attribute.GENERIC_MAX_HEALTH);
@@ -71,6 +85,11 @@ public class BloodHunt extends ContinuousAbilityItem {
 
             @Override
             public void onTick() {
+                if (targets == null || targets.isEmpty()) return;
+
+                for (PotionEffect potionEffect : potionEffects) {
+                    user.addPotionEffect(potionEffect);
+                }
             }
 
             @Override
