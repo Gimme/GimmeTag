92e81fcf1ceaab5b26769e0f6ac8039756141e9c
Anders Magnusson <anders14magnusson@gmail.com>
2020-10-17 05:01:03 +0200

    Make arrows the default projectile of bow items

--- src/main/java/me/gimme/gimmetag/item/BowProjectileItem.java
+++ src/main/java/me/gimme/gimmetag/item/BowProjectileItem.java
@@ -6,7 +6,9 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
@@ -15,12 +17,14 @@ import org.jetbrains.annotations.NotNull;
 public abstract class BowProjectileItem extends BouncyProjectileItem {
 
     private static final Material MATERIAL = Material.BOW;
+    private static final Class<? extends Projectile> PROJECTILE_CLASS = Arrow.class;
 
     public BowProjectileItem(@NotNull String id, @NotNull String displayName, @NotNull BouncyProjectileConfig config, @NotNull Plugin plugin) {
         super(id, displayName, MATERIAL, config, plugin);
 
         setUseEvent(UseEvent.SHOOT_BOW);
         setUseSound(new StandardSoundEffect(Sound.ENTITY_ARROW_SHOOT, SoundCategory.NEUTRAL));
+        setProjectileClass(PROJECTILE_CLASS);
     }
 
     @Override
